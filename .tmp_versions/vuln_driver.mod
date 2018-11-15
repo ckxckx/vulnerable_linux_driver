e4e6a16d7265bc9a0bc7786378f3bdd29c541234
ckxckx <ckx1025ckx@gmail.com>
2018-11-15 11:38:21 +0800

    tmp

---
+++ .tmp_versions/vuln_driver.mod
@@ -0,0 +1,2 @@
+/home/ckx/vulnerable_linux_driver/src/vuln_driver.ko
+/home/ckx/vulnerable_linux_driver/src/vuln_driver.o
---
+++ src/.vuln_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/ckx/vulnerable_linux_driver/src/vuln_driver.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/ckx/vulnerable_linux_driver/src/vuln_driver.ko /home/ckx/vulnerable_linux_driver/src/vuln_driver.o /home/ckx/vulnerable_linux_driver/src/vuln_driver.mod.o
